3b9fcf5c8c5fb9284bd89068465ac030f7940470
Cameron Lund <clund6314@gmail.com>
2021-06-22 19:59:12 -0600

    provided logic done

---
+++ client/src/components/shared/Dash.js
@@ -0,0 +1,5 @@
+import { Header } from 'semantic-ui-react';
+const Dash = () => (
+  <Header>Dash Page</Header>
+)
+export default Dash;
---
+++ client/src/components/shared/Home.js
@@ -0,0 +1,5 @@
+import { Header } from 'semantic-ui-react';
+const Home = () => (
+  <Header>Home Page</Header>
+)
+export default Home;
